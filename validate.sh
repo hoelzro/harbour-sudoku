0286d074b6ec65804afe0e64bdb600078370023a
Rob Hoelz <rob@hoelz.ro>
2014-04-30 08:42:44 -0500

    Fix up path for validation script

--- validate.sh
+++ validate.sh
@@ -4,4 +4,4 @@ function highlight {
     perl -MTerm::ANSIColor -npe 'BEGIN { %colors = ( PASSED => "green", WARNING => "yellow", ERROR => "red" ) } s/^(PASSED|WARNING|ERROR)/colored([$colors{$1}, "bold"], $1)/ge'
 }
 
-ssh -i ~/SailfishOS/vmshare/ssh/private_keys/engine/mersdk mersdk@localhost -p 2222 "cd $(pwd | perl -npe 's{$ENV{HOME}}{/home/mersdk}'); rpmvalidation-wrapper.sh -u RPMS/harbour-sudoku*.rpm" | highlight
+ssh -i ~/SailfishOS/vmshare/ssh/private_keys/engine/mersdk mersdk@localhost -p 2222 "cd $(pwd | perl -npe 's{$ENV{HOME}}{/home/mersdk/share}'); rpmvalidation-wrapper.sh -u RPMS/harbour-sudoku*.rpm" | highlight
