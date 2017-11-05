289f41b78b34e0c41727f979b29f8f15b912eac8
Rob Hoelz <rob@hoelz.ro>
2017-11-05 08:36:39 -0600

    Only connect to my phone if RPMS are OK

--- deploy.sh
+++ deploy.sh
@@ -13,9 +13,11 @@ function assert_jolla_connection {
 }
 
 assert_mer_running
-assert_jolla_connection
 
 ssh -i /var/extra/SailfishOS/vmshare/ssh/private_keys/engine/mersdk mersdk@localhost -p 2222 "cd $(pwd | perl -npe 's{$ENV{HOME}}{/home/mersdk/share}'); mb2 -t SailfishOS-armv7hl build; mv RPMS/*.rpm /tmp; mb2 -t SailfishOS-i486 build; mv /tmp/*.rpm RPMS"
 ssh -i /var/extra/SailfishOS/vmshare/ssh/private_keys/engine/mersdk mersdk@localhost -p 2222 "cd $(pwd | perl -npe 's{$ENV{HOME}}{/home/mersdk/share}'); rpmvalidation.sh RPMS/*armv7hl.rpm && rpmvalidation.sh RPMS/*i486.rpm"
+
+assert_jolla_connection
+
 scp -o ControlPath=/tmp/jolla.sock RPMS/harbour-sudoku*.armv7hl.rpm jolla:
 ssh -S /tmp/jolla.sock jolla 'devel-su pkcon --noninteractive install-local harbour-sudoku*.armv7hl.rpm'
